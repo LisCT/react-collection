9841bae3bf987412c53d60d06e5dbfc7b3b0fd84
Lisbel Cruz <Lcruztaveras@gmail.com>
2021-12-09 17:39:44 -0600

    adding popular styles

--- github-battle/app/components/Popular.js
+++ github-battle/app/components/Popular.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { fetchPopularApi } from '.././utils/api';
+import { FaUserAlt, FaStar, FaCodeBranch, FaExclamationTriangle } from 'react-icons/fa';
 
 function LanguagesNav ({ selected, onUpdateLanguage }) {
   const languages = ["all", "JavaScript", "Ruby", "Java", "CSS", "Phyton"];
 
   return(
-    <ul className="flex-center">
+    <ul className="filter flex-center">
     {languages.map(language => (
       <li key={ language }>
         <button
@@ -26,23 +28,100 @@ LanguagesNav.prototype = {
   onUpdateLanguage: PropTypes.func.isRequired
 }
 
+
+function Grid ({ selected }){
+  return(
+    <div className="box-wrapper">
+      {selected.map((info, index) => {
+        const { name, owner, stargazers_count, forks_count, open_issues } = info;
+        const { avatar_url, login } = owner;
+        return(
+          <div className="box" key={index}>
+            <div className="box-content">
+              <h1 className="box-header">#{index+1}</h1>
+              <img src={avatar_url} alt="name" className="box-image" />
+              <h3 className="box-subtitle">{name}</h3>
+              <div className="box-list">
+                <div className="box-item">
+                  <FaUserAlt className="box-icon" color="ffc072"/>
+                  <p>{login}</p>
+                </div>
+                <div className="box-item">
+                  <FaStar className="box-icon" color="fed609"/>
+                  <p>{stargazers_count}</p>
+                </div>
+                <div className="box-item">
+                  <FaCodeBranch className="box-icon" color="bcd9f4"/>
+                  <p>{forks_count}</p>
+                </div>
+                <div className="box-item">
+                  <FaExclamationTriangle className="box-icon" color="f48b96"/>
+                  <p>{open_issues}</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        )
+      })}
+    </div>
+  )
+}
+
+Grid.propTypes = {
+  selected: PropTypes.array.isRequired
+}
 export default class Popular extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      selectedLanguage: 'all'
+      selectedLanguage: 'all',
+      repos: {},
+      error: null
     }
 
     this.updatedLanguage = this.updatedLanguage.bind(this);
   }
 
   updatedLanguage (selectedLanguage) {
-    this.setState({ selectedLanguage });
+    this.setState({
+      selectedLanguage,
+      error: null
+    });
+
+    if(!this.state.repos[selectedLanguage]){
+
+      fetchPopularApi(selectedLanguage)
+      .then(data => {
+        this.setState(({repos}) => ({
+          repos:{
+            ...repos,
+            [selectedLanguage]: data
+          }
+        }))
+      })
+      .catch(error => {
+        console.warn('error fetching repos', error);
+
+        this.setState({
+          error: 'There was an error fetching popular api repositories.'
+        });
+
+      })
+    }
+  }
+
+  componentDidMount(){
+    this.updatedLanguage(this.state.selectedLanguage);
+  }
+
+  isLoading = () => {
+    const { selectedLanguage, repos, error } = this.state;
+    return !repos[selectedLanguage] && error === null;
   }
 
   render(){
 
-    const { selectedLanguage } = this.state;
+    const { selectedLanguage, repos, error } = this.state;
 
     return(
       <React.Fragment>
@@ -50,6 +129,13 @@ export default class Popular extends React.Component {
           selected={ selectedLanguage }
           onUpdateLanguage={ this.updatedLanguage }
         />
+
+        { this.isLoading() && <p>Loding...</p>}
+
+        { error && <p>{error}</p>}
+
+        { repos[selectedLanguage] && <Grid selected={ repos[selectedLanguage] }/> }
+
       </React.Fragment>
     )
   }
